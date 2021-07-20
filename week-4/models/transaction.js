9cd174c19f507d4a2f7130f5b5bb5541da7f96dc
Joel Detweiler <joeldd@uw.edu>
2021-07-20 16:46:47 -0700

    Convert schema type to ObjectId

--- week-4/models/transaction.js
+++ week-4/models/transaction.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const transactionSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   charge: { type: String, required: true },
-  userId: { type: String, required: true, index: true }
+  userId: { type: mongoose.Types.ObjectId, required: true, index: true }
 });
 
 module.exports = mongoose.model("transactions", transactionSchema);
